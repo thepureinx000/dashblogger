3f85bfd9408416039c89b7877e1b0b3e0c104d1a
thepureinx <laszlo.develop@gmail.com>
2017-09-05 00:22:56 +0300

    updating architecture

--- src/App.js
+++ src/App.js
@@ -5,14 +5,21 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="container">
+        <div className="card card-container">
+            <img className="profile-img-card" src="//lh3.googleusercontent.com/-6V8xOA6M7BA/AAAAAAAAAAI/AAAAAAAAAAA/rzlHcD0KYwo/photo.jpg?sz=120" alt="" />
+            <img id="profile-img" className="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+            <p id="profile-name" className="profile-name-card"></p>
+            <form className="form-signin">
+                <span id="reauth-email" className="reauth-email"></span>
+                <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
+                <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
+                <button className="btn btn-lg btn-primary btn-block btn-signin" type="submit">Sign in</button>
+            </form>
+            <a href="#" className="forgot-password">
+                Forgot the password?
+            </a>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
