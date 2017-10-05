525e5a53e3543d6700e77163bc910ddf608759c5
thepureinx <laszlo.develop@gmail.com>
2017-10-06 00:57:29 +0300

    fix-components-2

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './media/styles/index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
 import { BrowserRouter, Router } from 'react-router-dom';
-
 import routes from './routes';
 
 const component = (
